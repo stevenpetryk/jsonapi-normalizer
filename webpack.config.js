920776ec72ff699185bd610ae5de04319c588d36
Steven Petryk <me@stevenpetryk.com>
2016-11-16 13:47:03 -0500

    Prepare for publishing (#3)
    * Prepare for publishing
    
    * Remove unused babel plugin

---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+var webpack = require('webpack')
+
+module.exports = {
+  entry: './src/index',
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        loader: 'babel',
+        exclude: /node_modules/
+      }
+    ]
+  },
+  output: {
+    filename: 'dist/index.js',
+    libraryTarget: 'umd',
+    library: 'jsonapi-normalizer'
+  },
+  plugins: [
+    new webpack.optimize.OccurenceOrderPlugin(),
+    new webpack.DefinePlugin({
+      'process.env': {
+        'NODE_ENV': JSON.stringify('production')
+      }
+    }),
+    new webpack.optimize.UglifyJsPlugin({
+      compressor: {
+        warnings: false
+      }
+    })
+  ]
+}
