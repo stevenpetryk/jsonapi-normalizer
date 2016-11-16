723c5eccc9e82fea0f1e4f7bb8faa1f0db1f56c5
Steven Petryk <me@stevenpetryk.com>
2016-11-16 13:34:38 -0500

    Configure CI (#2)
    * Add Circle config
    
    * Improve testing
    
    * Add sexy badges
    
    * Ensure standard is installed

--- src/utils.js
+++ src/utils.js
@@ -1,6 +1,6 @@
 export function compose (...functions) {
   return (input) => {
-    functions.forEach(func => input = func(input))
+    functions.forEach(func => { input = func(input) })
     return input
   }
 }
