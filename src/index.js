5708738259732035eff11a74e93197aab0ee09f0
Steven Petryk <me@stevenpetryk.com>
2016-11-18 18:22:34 -0500

    Refactor main module (#4)

--- src/index.js
+++ src/index.js
@@ -1,75 +1,47 @@
-import { compose } from './utils'
-
 module.exports = function (response) {
   const data = response.data || []
   const included = response.included || []
 
-  const transformer = compose(extractRelationships, flattenResource)
+  var allResources = [...data, ...included]
+  var entities = {}
 
-  return {
-    entities: mergeEachKey(
-      categorizeByType(data, transformer),
-      categorizeByType(included, transformer)
-    )
+  allResources.forEach(entity => addEntity(entities, entity))
+
+  return { entities }
+}
+
+function addEntity (entities, entity) {
+  const { type, id, attributes } = entity
+
+  if (!entities[type]) entities[type] = {}
+
+  entities[type][id] = {
+    id,
+    ...attributes,
+    ...extractRelationships(entity)
   }
+
+  return entities
 }
 
-function mergeEachKey (object1, object2) {
-  var result = {}
+function extractRelationships (entity) {
+  const { relationships: responseRelationships } = entity
 
-  const allKeys = Object.keys(object1).concat(Object.keys(object2))
-
-  allKeys.forEach((key) => {
-    result[key] = {
-      ...object1[key],
-      ...object2[key]
-    }
-  })
-
-  return result
-}
-
-function categorizeByType (resources, transformer) {
-  var types = {}
-
-  resources.forEach(resource => (
-    types[resource.type] = {
-      ...types[resource.type],
-      [resource.id]: transformer(resource)
-    }
-  ))
-
-  return types
-}
-
-function extractRelationships (resource) {
-  if (!resource.relationships) return resource
+  if (!responseRelationships) return undefined
 
   var relationships = {}
 
-  Object.keys(resource.relationships).map(type => {
-    const theseRelationships = resource.relationships[type].data
-
-    if (Array.isArray(theseRelationships)) {
-      relationships[type] = [ ...theseRelationships ]
-    } else {
-      relationships[type] = { ...theseRelationships }
-    }
+  Object.keys(responseRelationships).map(type => {
+    relationships[type] = duplicateRelationships(responseRelationships[type].data)
   })
 
-  return {
-    id: resource.id,
-    type: resource.type,
-    attributes: {
-      ...resource.attributes,
-      ...relationships
-    }
-  }
+  return relationships
 }
 
-function flattenResource (resource) {
-  return {
-    id: resource.id,
-    ...resource.attributes
+function duplicateRelationships (relationships) {
+  if (Array.isArray(relationships)) {
+    return [ ...relationships ]
+  } else {
+    return { ...relationships }
   }
 }
--- src/utils.js
+++
@@ -1,6 +0,0 @@
-export function compose (...functions) {
-  return (input) => {
-    functions.forEach(func => { input = func(input) })
-    return input
-  }
-}
