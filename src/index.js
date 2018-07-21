17bb78595166885826b02ba69a293f2e5aa73168
Steven Petryk <petryk.steven@gmail.com>
2018-07-21 15:43:31 -0700

    Don't deprecate default import

--- src/index.js
+++ src/index.js
@@ -1,13 +1,3 @@
 import normalize from './normalize'
 
-export default function (...args) {
-  console.warn(`
-Directly importing 'normalize' is deprecated. Please use the named export:
-
-  import { normalize } from 'json-api-normalizer'
-`)
-
-  return normalize(...args)
-}
-
-export { normalize }
+export default normalize
