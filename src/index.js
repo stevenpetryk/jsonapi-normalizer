4e962d5ace91b9c56733d00bf240973945cc8fae
Steven Petryk <petryk.steven@gmail.com>
2018-07-21 15:49:33 -0700

    Fix default exports bullshit

--- src/index.js
+++ src/index.js
@@ -1,3 +1,72 @@
-import normalize from './normalize'
+module.exports = function (response) {
+  let data
 
-export default normalize
+  if (Array.isArray(response.data)) {
+    data = response.data
+  } else {
+    data = [ response.data ]
+  }
+
+  const included = response.included || []
+
+  let result = {}
+  let entities = {}
+
+  data.forEach(entity => {
+    addResult(result, entity)
+    addEntity(entities, entity)
+  })
+
+  included.forEach(entity => {
+    addEntity(entities, entity)
+  })
+
+  return {
+    result,
+    entities
+  }
+}
+
+function addResult (result, entity) {
+  const { type, id } = entity
+
+  if (!result[type]) result[type] = []
+
+  result[type].push(id)
+}
+
+function addEntity (entities, entity) {
+  const { type, id, attributes } = entity
+
+  if (!entities[type]) entities[type] = {}
+
+  entities[type][id] = {
+    id,
+    ...attributes,
+    ...extractRelationships(entity)
+  }
+
+  return entities
+}
+
+function extractRelationships (entity) {
+  const { relationships: responseRelationships } = entity
+
+  if (!responseRelationships) return undefined
+
+  let relationships = {}
+
+  Object.keys(responseRelationships).map(type => {
+    relationships[type] = duplicateRelationships(responseRelationships[type].data)
+  })
+
+  return relationships
+}
+
+function duplicateRelationships (relationships) {
+  if (Array.isArray(relationships)) {
+    return [ ...relationships ]
+  } else {
+    return { ...relationships }
+  }
+}
